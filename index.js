db15759b949dfb9027e5ea27e491981a4b29e352
Cristian Giraldo Zúñiga <cristian.zuniga@rappi.com>
2022-11-22 14:50:43 -0500

    windows release

---
+++ index.js
@@ -0,0 +1,30 @@
+const robot = require("robotjs");
+
+while(true){
+    robot.setMouseDelay(1000);
+
+    robot.moveMouse(1484, 366);
+    robot.mouseClick();
+
+    robot.setMouseDelay(500);
+
+    robot.moveMouse(1484, 466);
+    robot.mouseClick();
+
+    robot.moveMouse(1484, 566);
+    robot.mouseClick();
+
+    robot.moveMouse(1484, 666);
+    robot.mouseClick();
+
+    robot.moveMouse(1484, 766);
+    robot.mouseClick();
+
+    robot.moveMouse(1484, 866);
+    robot.mouseClick();
+
+    robot.setMouseDelay(1000);
+
+    robot.moveMouse(289, 530);
+    robot.mouseClick();
+}
